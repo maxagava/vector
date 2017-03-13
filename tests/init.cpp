eb72dece0a044d6bc2535741e302b57fedf95fe2
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-03-13 14:18:25 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,3 +6,10 @@ SCENARIO("vector init", "[init]") {
 	REQUIRE(vector.size() == 0);
 	REQUIRE(vector.capacity() == 0);
 }
+SCENARIO("vector with param", "[init]") {
+	vector_t vector(3);
+	REQUIRE(vector.size() == 3);
+	REQUIRE(vector.capacity() == 3);
+	for(unsigned int index = 0; index < vector.size(); ++ index){
+		REQUIRE(vector[index]==0);
+}
